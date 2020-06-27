c356faee7c27afa602156cf07489350491691573
Paul Rupke <31414339+prupke2@users.noreply.github.com>
2020-06-27 18:49:48 -0400

    Added flask backend, added test /time endpoint (#1)
    * added /api folder and set up a flask environment for the back end
    * added a test `/time` endpoint and called from the front end to confirm it works
    * deleted commented code from refactored project
    * small updates to package.json and .gitignore
    * set PlayoffTree to a const instead of a class

--- src/components/PlayoffTree/PlayoffTree.jsx
+++ src/components/PlayoffTree/PlayoffTree.jsx
@@ -2,78 +2,36 @@ import React, { Component } from 'react';
 import Teams from './Conference/Teams';
 import './playoffTree.css';
 
-// var data = {
-//     content: {
-//         people: [
-//           {
-//             name: "Test",
-//             age : 24
-//           },
-//           {
-//             name: "Foo",
-//             age: 25
-//           }
-//         ]
-//      }
-//   };
-  
-//   var App = React.createClass({
-//     render: function() {
-//       var people = data.content.people.map(function(person){
-//         return (<div>{person.name}</div>);
-//       });
-  
-//       return (<div>{people}</div>)
-//     }
-//   }); 
+const PlayoffTree = (props) => {
 
-// var Teams = React.createClass({
-//     render: function() {
-//         var teams = teamsList.teams.map(function(team) {
-//             return (
-//                 <div>
-//                     {team.name}
-//                 </div>
-//             )
-//         });
-
-//         return (<div>{teams}</div>)
-//     }
-// });
-
-// const PlayoffTree = (props) => {
-
-
-//     return (
-//         <section className="Playoff-tree">
-//             <div>{props}
-//             </div>
-//         </section>
-//     )
-// }
-
-class PlayoffTree extends Component {
-    static getDerivedStateFromProps(props, state) {
-		// console.log({props});
-		return state;
-    }
+    // static getDerivedStateFromProps(props, state) {
+	// 	// console.log({props});
+	// 	return state;
+    // }
     
-    render() {
 		return (
 			<div className="PlayoffTree">
 				<section>
 					<Teams 
-						teams={this.props.eastTeams}>
+						teams={props.eastTeams}>
 					</Teams>
+				</section>
+				<section style={{width: "150px;"}}>
+					<button
+						onClick={props.getTime}
+					>Get time
+					</button>
+					<p>Current Time: {props.currentTime}</p>
 				</section>	
 				<section>
 					<Teams 
-						teams={this.props.westTeams}>
+						teams={props.westTeams}>
 					</Teams>
 				</section>
+
 			</div>
 		);
-    }
+    
 }
 
 export default React.memo(PlayoffTree);
