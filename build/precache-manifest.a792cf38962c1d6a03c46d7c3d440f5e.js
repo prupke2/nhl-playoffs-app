9d531f75003ea705ad3efc8401a4620e3f9b3264
Paul Rupke <paul.rupke@gmail.com>
2020-09-07 18:08:40 -0400

    New build with final 4

---
+++ build/precache-manifest.a792cf38962c1d6a03c46d7c3d440f5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af381e91dce6f2c6ec6f621e7c11f439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eb4fbe967cf4fa40a50",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "0cad971839ac60a00d1b",
+    "url": "/static/css/main.b1a4ba7e.chunk.css"
+  },
+  {
+    "revision": "4eb4fbe967cf4fa40a50",
+    "url": "/static/js/2.1d5eef0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d5eef0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cad971839ac60a00d1b",
+    "url": "/static/js/main.2a2e4153.chunk.js"
+  },
+  {
+    "revision": "dffce04f1f2bdae5970a",
+    "url": "/static/js/runtime-main.48eaae6e.js"
+  },
+  {
+    "revision": "cc443e141fbb73f882bea645a244e715",
+    "url": "/static/media/hockey_icon_large.cc443e14.png"
+  }
+]);
